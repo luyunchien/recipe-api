a195a7cf5b92657b2a0cc4ff6b9fca147132282a
Yun-Chien Lu <luyunchien@gmail.com>
2022-12-07 18:17:34 +0000

    did some cool things 2

--- src/main/java/com/roinnsenn/recipe/api/RecipeController.java
+++ src/main/java/com/roinnsenn/recipe/api/RecipeController.java
@@ -2,10 +2,13 @@ package com.roinnsenn.recipe.api;
 
 import com.roinnsenn.recipe.api.model.CreateRecipe;
 import com.roinnsenn.recipe.api.model.RecipeResponse;
+import com.roinnsenn.recipe.api.model.UpdateRecipe;
 import com.roinnsenn.recipe.entity.Recipe;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +27,8 @@ public class RecipeController {
     }
 
     @GetMapping("/{id}")
-    public String getRecipe(@PathVariable int id){
-        return "todo - get recipe " + id;
+    public RecipeResponse getRecipe(@PathVariable int id){
+        return RecipeResponse.fromEntity(findByID(id));
     }
 
     @PostMapping(value = "/")
@@ -34,21 +37,39 @@ public class RecipeController {
                 .name(createRecipe.getName())
                 .ingredients(createRecipe.getIngredients())
                 .steps(createRecipe.getSteps())
-                .id(0)
+                .id(nextID())
                 .build();
         recipeStore.add(recipe);
         return RecipeResponse.fromEntity(recipe);
     }
 
     @PutMapping("/{id}")
-    public String updateRecipe(@PathVariable int id){
-        return "todo - update recipe " + id;
+    public RecipeResponse updateRecipe(@PathVariable int id, @RequestBody @Validated UpdateRecipe updateRecipe){
+        var entity = findByID(id);
+        if(updateRecipe.getName()!=null){
+            entity.setName(updateRecipe.getName());
+        }
+        if(updateRecipe.getIngredients()!=null){
+            entity.setIngredients(updateRecipe.getIngredients());
+        }
+        if(updateRecipe.getSteps()!=null){
+            entity.setSteps(updateRecipe.getSteps());
+        }
+        return RecipeResponse.fromEntity(entity);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteRecipe(@PathVariable int id){
-        return "todo - delete recipe " + id;
+    public void deleteRecipe(@PathVariable int id){
+        var entity = findByID(id);
+        recipeStore.remove(entity);
     }
 
+    private int nextID(){
+        return recipeStore.stream().mapToInt(recipe -> recipe.getId()).max().orElse(0) + 1;
+    }
 
+    private Recipe findByID(int id){
+        return recipeStore.stream().filter((recipe -> recipe.getId()==id)).findFirst().orElseThrow(()->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "entity doesn't exist"));
+    }
 }
